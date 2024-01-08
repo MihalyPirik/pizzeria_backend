d26e6a931b72141a3e50617ba4da522a28264919
Mihaly Pirik <misipirik@gmail.com>
2024-01-08 06:29:28 +0100

    basic routes

---
+++ app/Http/Controllers/FoodController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Food;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $foods = Food::all();
+        //$Foods = Food::with('category')->get();
+        return response()->json($foods);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id) {
+        //$Food = Food::with('category')->find($id);
+        $food = Food::find($id);
+        if ($food == null) {
+            return response()->json(['message' => 'No food found'], 404);
+        }
+        return response()->json($food);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Food $food)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Food $food)
+    {
+        //
+    }
+}
